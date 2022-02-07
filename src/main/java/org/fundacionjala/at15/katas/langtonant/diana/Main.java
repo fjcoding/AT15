1b61d7a8e0facf8218530fd9267bb55362872f4c
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-02-07 16:09:02 -0400

    diana example

---
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/diana/Main.java
@@ -0,0 +1,8 @@
+package org.fundacionjala.at15.katas.langtonant.diana;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("FROM DIANA: This is Langton's Ant main class, you can do yout implementation here :)");
+    }
+}
