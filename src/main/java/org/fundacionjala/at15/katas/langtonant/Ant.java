170518e5e18e4a02abe25d7fdca7a775ecb5a57c
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-02-07 15:19:18 -0300

    added class Ant, getters & setters

--- src/main/java/org/fundacionjala/at15/katas/langtonant/Ant.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/Ant.java
@@ -2,4 +2,49 @@ package org.fundacionjala.at15.katas.langtonant;
 
 public class Ant {
     // TODO: Make Ant Class with constructor and methods
+
+    // variables
+    int xPos;
+    int yPos;
+    // direction
+    String direction;
+    Grid g;
+    boolean isFinished;
+
+    public Ant(int xPos, int yPos, String direction, Grid g) {
+        // constructor
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.direction = direction;
+        this.g = g;
+    }
+
+    // Getter coordinates
+
+
+    public int getXPos() {
+        return xPos;
+    }
+
+    public int getYPos() {
+        return yPos;
+    }
+
+    // Setters
+
+    public void setXPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public void setYPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public void run() {
+        //TODO: while(!isFinished) determine direction, change tile color
+    }
 }
