55e6077715a0e69a593b50f4d65f08594808510d
Sergio <Sergio.Mendieta@fundacion-jala.org>
2022-02-07 17:11:30 -0400

    test2

--- src/main/java/org/fundacionjala/at15/katas/langtonant/sergio/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/sergio/Main.java
@@ -3,6 +3,6 @@ package org.fundacionjala.at15.katas.langtonant.sergio;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is Langton's Ant main class, you can do yout implementation here :)");
+        System.out.println("Hello World");
     }
 }
