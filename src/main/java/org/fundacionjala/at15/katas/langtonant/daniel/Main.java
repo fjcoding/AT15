b25539f3e3da1cad03ca75b9194aa8202fa92cb6
Daniel <Daniel.Baspineiro@fundacion-jala.org>
2022-02-07 16:53:43 -0400

    add daniel/langtonant

---
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/daniel/Main.java
@@ -0,0 +1,8 @@
+package org.fundacionjala.at15.katas.daniel.langtonant;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is Langton's Ant main class, you can do yout implementation here Daniel :)");
+    }
+}
