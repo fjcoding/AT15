4011d8a5bfd0b4adbeabf8476497e6cde2cd9d7a
Saul Fuentes <saul.fuentes@fundacion-jala.org>
2022-02-07 16:06:53 -0400

    Start Langtonant Kata in Main.java

---
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/saul/Main.java
@@ -0,0 +1,9 @@
+package org.fundacionjala.at15.katas.langtonant.saul;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Saul here.... This is Langton's Ant main class, you can do yout implementation here :)");
+    }
+}
+
