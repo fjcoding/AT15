57b482d94c53a36c1817601b333eb1c58183bebc
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-02-07 16:42:14 -0400

    Create the structure to solve my kata

---
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/adhemar/Main.java
@@ -0,0 +1,8 @@
+package org.fundacionjala.at15.katas.langtonant.adhemar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test Adhemar");
+    }
+}
