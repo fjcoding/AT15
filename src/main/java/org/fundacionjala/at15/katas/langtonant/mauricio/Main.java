e44d8c7cf08ab0634126fab5ef73177145aa9d59
Mauricio Davalos Llave <Mauricio.Davalos@fundacion-jala.org>
2022-02-07 16:14:05 -0400

    Mauricio example

---
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/mauricio/Main.java
@@ -0,0 +1,8 @@
+package org.fundacionjala.at15.katas.langtonant.mauricio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("From Mauricio: This is Langton's Ant main class, you can do yout implementation here :)");
+    }
+}
