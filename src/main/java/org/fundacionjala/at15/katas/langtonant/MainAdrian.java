4c05d4bd20dd719f9b85c48d1d9354e6f12ecd81
Adrian Oviedo <Adrian.Oviedo@fundacion-jala.org>
2022-02-07 12:38:31 -0400

    Added Adrian task to build.gradle

---
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/MainAdrian.java
@@ -0,0 +1,8 @@
+package org.fundacionjala.at15.katas.langtonant;
+
+public class MainAdrian {
+    public static void main(String[] args) {
+        System.out.println("Ejemplo de Task de Adrian");
+    }
+    
+}
