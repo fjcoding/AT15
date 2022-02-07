3ea0e85084b8bb1d27f318d623cfa72d6b35adb4
Sergio <Sergio.Mendieta@fundacion-jala.org>
2022-02-07 18:34:23 -0400

    class Grid added

---
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/sergio/Grid.java
@@ -0,0 +1,57 @@
+package org.fundacionjala.at15.katas.langtonant.sergio;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class Grid {
+    //atributos
+    private boolean [][] grid;
+    private int height;
+    private int width;
+
+    //constructor
+    public Grid(int height, int width){
+        this.height = height;
+        this.width = width;
+        grid = new boolean [height][width];
+    }
+
+    //metodos 
+    //getters
+
+    public int getHeight(){
+        return height;
+    }
+    public int getWidth(){
+        return width;
+    }
+    public boolean[][] getGrid(){
+        return grid;        
+    }
+    public boolean isWhite(int h,int w){
+        return grid[h][w];
+    }
+    //setters
+
+    private void setWhite(int h, int w){
+        if(h<height && w<width){
+            grid[h][w]=true;
+        }        
+    }
+    private void setBlack(int h, int w){
+        if(h<height && w<width){
+            grid[h][w]=false;
+        }
+    }
+    public void printGrid(){
+        for(int h=0;h<height;h++){
+            for (int w=0;w<width;w++){
+                if (isWhite(h,w)){
+                    System.out.print(" ");
+                }else {
+                    System.out.print("x");
+                }
+            }System.out.println();
+        }
+    }
+    
+}
--- src/main/java/org/fundacionjala/at15/katas/langtonant/sergio/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/sergio/Main.java
@@ -3,6 +3,8 @@ package org.fundacionjala.at15.katas.langtonant.sergio;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       // System.out.println("Hello World");
+       Grid grid = new Grid(5,7);
+       grid.printGrid();
     }
 }
