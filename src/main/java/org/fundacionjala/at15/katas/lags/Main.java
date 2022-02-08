5e2073cbf78cb66883b2c0f978cea543bd70fde6
Gonzalo Encinas <Gonzalo.Encinas@fundacion-jala.org>
2022-02-08 16:40:59 -0400

    add new variables

--- src/main/java/org/fundacionjala/at15/katas/lags/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/lags/Main.java
@@ -2,25 +2,17 @@ package org.fundacionjala.at15.katas.lags;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("aloha");
-
-        rentRequest r1 = new rentRequest(0, 5, 10, "AF514");
-        rentRequest r2 = new rentRequest(3, 7, 14, "CO5");
-        rentRequest r3 = new rentRequest(5, 9, 7, "AF515");
-        rentRequest r4 = new rentRequest(6, 9, 8, "BA01");
-
-        rentRequest[] array = {r1, r2, r3, r4};
-
-        System.out.println(bestCombination(array));
-    }
-
-    public static int bestCombination(rentRequest[] array){
-        int bestCombination = 0;
-
-        
-        // put your code here
-
-        return bestCombination;
+        rentRequest []rent = new rentRequest[4];     
+        rent[0]=new rentRequest(0,5,10,"AF514");
+		rent[1]=new rentRequest(3,7,14,"CO5");
+		rent[2]=new rentRequest(5,9,7,"AF515");
+		rent[3]=new rentRequest(6,9,8,"BA01");
+        int SD,S;
+        int sum=0;
+        int fly1=0,fly2=0;
+        for (int i = 0; i < rent.length; i++) {
+	
+		}      
     }
 
 }
