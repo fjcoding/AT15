579ff29801cab3265a46be99372c8d23933a334a
Sergio <Sergio.Mendieta@fundacion-jala.org>
2022-02-07 21:22:58 -0400

    main to run added

--- src/main/java/org/fundacionjala/at15/katas/langtonant/sergio/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/sergio/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
        // System.out.println("Hello World");
        Grid grid = new Grid(5,7);
+       
+       Ant ant = new Ant(3,4,"up",grid);
+       ant.run();
+
+       System.out.println("Final Position: ");
        grid.printGrid();
     }
 }
