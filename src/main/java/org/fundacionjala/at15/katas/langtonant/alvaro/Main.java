63a54c132aaa55502f35a496eea645169c4b95fc
Alvaro Sivila <Alvaro.Sivila@fundacion-jala.org>
2022-02-07 20:24:51 -0400

    Langtonant exercise

--- src/main/java/org/fundacionjala/at15/katas/langtonant/alvaro/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/alvaro/Main.java
@@ -1,8 +1,20 @@
 package org.fundacionjala.at15.katas.langtonant.alvaro;
 
-public class Main {
 
+public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("Test");
+        //System.out.println("Saul here.... This is Langton's Ant main class, you can do yout implementation here :)");
+
+        Grid grid = new Grid(8, 8);
+        Ant ant = new Ant(2, 2, grid, "up");
+
+        //grid.printGrid();
+        ant.game(5);
+
+        //System.out.println(grid.getRows());
+        //System.out.println(grid.getColumns());
     }
+
+
 }
