7cb5b6ab81aec5318ada5871e32840eb74e8adea
vladimir <vla.evtm@gmail.com>
2022-02-07 21:39:07 -0400

    Add Langton Ant to vladimir/langtonant branch

---
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/vladimir/Main.java
@@ -0,0 +1,96 @@
+package org.fundacionjala.at15.katas.langtonant.vladimir;
+
+import java.util.*;
+
+public class Main {
+	static String[][] mat;
+	public static void main(String[] args) {
+			System.out.println("This is Langton's Ant main class, you can do yout implementation here :)");
+			//Scanner scanner=new Scanner(System.in);
+			int x = 11;
+			int y = 11;
+			mat = new String[x][y];
+			for (int i = 0; i < x; i++)
+			{
+					for (int j = 0; j < y; j++)
+					{
+						mat[i][j] = "W";
+					}
+			}
+			int posx = 5;
+			int posy = 5;
+			String dir = "U";
+			int rep = 10;
+			for (int i = 0; i < rep; i++)
+			{
+				if (mat[posx][posy] == "W")
+				{
+					switch (dir)
+					{
+						case "U":
+							dir = "R";
+							mat[posx][posy] = "B";
+							posy += 1;
+							break;
+						case "D":
+							dir = "L";
+							mat[posx][posy] = "B";
+							posy -= 1;
+							break;
+						case "L":
+							dir = "U";
+							mat[posx][posy] = "B";
+							posx -= 1;
+							break;
+						case "R":
+							dir = "D";
+							mat[posx][posy] = "B";
+							posx += 1;
+							break;
+					}
+				}
+				else if (mat[posx][posy] == "B")
+				{
+					switch (dir)
+					{
+						case "U":
+							dir = "L";
+							mat[posx][posy] = "W";
+							posy -= 1;
+							break;
+						case "D":
+							dir = "R";
+							mat[posx][posy] = "W";
+							posy += 1;
+							break;
+						case "L":
+							dir = "D";
+							mat[posx][posy] = "W";
+							posx += 1;
+							break;
+						case "R":
+							dir = "U";
+							mat[posx][posy] = "W";
+							posx -= 1;
+							break;
+					}
+				}
+				print(x, y);
+			}
+	}
+	private static void print(int x, int y)
+		{
+			for (int i = 0; i < x; i++)
+			{
+				for (int j = 0; j < y; j++)
+				{
+					System.out.print(mat[i][j] + " ");
+				}
+				System.out.println();
+			}
+			System.out.println("---------------------------------------");
+			System.out.println();
+
+		}
+}
+
