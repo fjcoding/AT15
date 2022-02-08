37a9b6d512fe91e5a89e9ae7a3b3f5f16ba3c027
Saul Fuentes <saul.fuentes@fundacion-jala.org>
2022-02-08 15:43:08 -0400

    Add bestCombinacion method and declare rentRequest in Main Class

--- src/main/java/org/fundacionjala/at15/katas/lags/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/lags/Main.java
@@ -2,6 +2,25 @@ package org.fundacionjala.at15.katas.lags;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("aloha");
+        //System.out.println("aloha");
+
+        rentRequest r1 = new rentRequest(0, 5, 10, "AF514");
+        rentRequest r2 = new rentRequest(3, 7, 14, "CO5");
+        rentRequest r3 = new rentRequest(5, 9, 7, "AF515");
+        rentRequest r4 = new rentRequest(6, 9, 8, "BA01");
+
+        rentRequest[] array = {r1, r2, r3, r4};
+
+        System.out.println(bestCombination(array));
     }
+
+    public static int bestCombination(rentRequest[] array){
+        int bestCombination = 0;
+
+        
+        // put your code here
+
+        return bestCombination;
+    }
+
 }
