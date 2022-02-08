f126187cc4898a9a13bb485ddbf5c4cb77ce6481
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-02-07 23:53:32 -0400

    Add Langtonant program file - Diana

---
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/diana/Ant.java
@@ -0,0 +1,139 @@
+package org.fundacionjala.at15.katas.langtonant.diana;
+
+public class Ant {
+    private int posX;
+    private int posY;
+    private Grid grid;
+    private String direction;
+    private boolean finished;
+
+    public Ant(int x, int y, Grid grid, String direction) {
+        posX = x;
+        posY = y;
+        this.grid = grid;
+        this.direction = direction;
+        finished = false; // the game is ready
+    }
+
+    // 0 -> negro (left)
+    // 1 -> blanco (right)
+    public void nextStep() {
+        switch (direction) {
+            case "up":
+                // System.out.println("UP");
+                if (grid.getSquare(posX, posY) == 1) { // es blanco?
+                    if (isValidNextPosition(posX, posY + 1, "right")) {
+                        grid.changeSquare(posX, posY);
+                        posY++;
+                        this.direction = "right";
+                    } else
+                        finished = true;
+                } else {
+                    if (isValidNextPosition(posX, posY - 1, "left")) {
+                        grid.changeSquare(posX, posY);
+                        posY--;
+                        this.direction = "left";
+                    } else
+                        finished = true;
+                }
+                break;
+            case "right":
+                // System.out.println("entro RIGHT");
+                if (grid.getSquare(posX, posY) == 1) {
+                    if (isValidNextPosition(posX + 1, posY, "down")) {
+                        grid.changeSquare(posX, posY);
+                        posX++;
+                        direction = "down";
+                    } else
+                        finished = true;
+                } else {
+                    if (isValidNextPosition(posX - 1, posY, "up")) {
+                        grid.changeSquare(posX, posY);
+                        posX--;
+                        direction = "up";
+                    } else
+                        finished = true;
+                }
+                break;
+
+            case "left":
+                if (grid.getSquare(posX, posY) == 1) {
+                    if (isValidNextPosition(posX - 1, posY, "up")) {
+                        grid.changeSquare(posX, posY);
+                        posX--;
+                        direction = "up";
+                    } else
+                        finished = true;
+                } else {
+                    if (isValidNextPosition(posX + 1, posY, "down")) {
+                        grid.changeSquare(posX, posY);
+                        posX++;
+                        direction = "down";
+                    } else
+                        finished = true;
+                }
+                break;
+
+            case "down":
+                if (grid.getSquare(posX, posY) == 1) {
+                    if (isValidNextPosition(posX, posY - 1, "left")) {
+                        grid.changeSquare(posX, posY);
+                        posY--;
+                        direction = "left";
+                    } else
+                        finished = true;
+                } else {
+                    if (isValidNextPosition(posX, posY + 1, "right")) {
+                        grid.changeSquare(posX, posY);
+                        posY++;
+                        direction = "right";
+                    } else
+                        finished = true;
+                    break;
+
+                }
+        }
+
+    }
+
+    public boolean isValidNextPosition(int x, int y, String dir) {
+        boolean flag = false;
+
+        switch (dir) {
+            case "up":
+                if (x > 0)
+                    flag = true;
+                break;
+
+            case "left":
+                if (y > 0)
+                    flag = true;
+                break;
+
+            case "right":
+                if (y < grid.getColumns() - 1)
+                    flag = true;
+                break;
+
+            case "down":
+                if (x < grid.getRows() - 1)
+                    flag = true;
+                break;
+        }
+        return flag;
+    }
+
+    public void game(int n) {
+        grid.printGrid();
+        System.out.println();
+        while (n > 0) {
+            if (finished == false) {
+                nextStep();
+                n--;
+            }
+            System.out.println();
+            grid.printGrid();
+        }
+        System.out.println("The game is over...");
+    }
+}
--- src/main/java/org/fundacionjala/at15/katas/langtonant/diana/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/diana/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("FROM DIANA: This is Langton's Ant main class, you can do yout implementation here :)");
+
+        Grid grid = new Grid(5, 10); // definicion tamaño de matriz
+        Ant ant = new Ant(3, 2, grid, "up"); // posicion y direccion de la hormiga
+        ant.game(3); // numero de saltos de la hormiga
+        System.out.println(grid.getRows());
+        System.out.println(grid.getColumns());
     }
 }
