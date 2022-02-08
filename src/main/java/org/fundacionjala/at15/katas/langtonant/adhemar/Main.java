1b10460738cb515b98378b8a62e708ae79bfc77f
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-02-07 21:00:35 -0400

    Sketch the main logic

--- src/main/java/org/fundacionjala/at15/katas/langtonant/adhemar/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/adhemar/Main.java
@@ -1,8 +1,15 @@
 package org.fundacionjala.at15.katas.langtonant.adhemar;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Test Adhemar");
+        //Inputs
+        //Enter grid's size
+        //Enter ant's initial position
+        //Enter the number of steps
+
+        //Create a grid
+        //Move the ant
+        //Show grid status
+
     }
 }
