d330a4df56f58c362511823166bdf7dd37607c55
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-02-08 08:58:27 -0400

    Fisrt part of the constructor of lags file

--- src/main/java/org/fundacionjala/at15/katas/lags/rentRequest.java
+++ src/main/java/org/fundacionjala/at15/katas/lags/rentRequest.java
@@ -1,5 +1,21 @@
 package org.fundacionjala.at15.katas.lags;
 
 public class rentRequest {
-    
+    int startTime;
+    int travelDuration;
+    int price;
+    String nameTravel;
+
+    public rentRequest(int startTime, int travelDuration, int price, String nameTravel){
+        this.startTime=startTime;
+        this.travelDuration=travelDuration;
+        this.price=price;
+        this nameTravel=nameTravel;
+
+        
+
+
+
+    }
+
 }
