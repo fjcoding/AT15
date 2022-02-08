15849f637ecfb5fbdbe8f259190f5cd3974e9857
Daniel <Daniel.Baspineiro@fundacion-jala.org>
2022-02-07 22:44:19 -0400

    langtonant problem

--- src/main/java/org/fundacionjala/at15/katas/langtonant/daniel/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/daniel/Main.java
@@ -1,8 +1,21 @@
 package org.fundacionjala.at15.katas.daniel.langtonant;
 
-public class Main {
+//import org.fundacionjala.at15.katas.langtonant.saul.*;
 
+public class Main {
+    
     public static void main(String[] args) {
-        System.out.println("This is Langton's Ant main class, you can do yout implementation here Daniel :)");
+        //System.out.println("Saul here.... This is Langton's Ant main class, you can do yout implementation here :)");
+
+        Grid grid = new Grid(8, 8);
+        Ant ant = new Ant(2, 2, grid, "up");
+
+        //grid.printGrid();
+        ant.game(5);
+
+        //System.out.println(grid.getRows());
+        //System.out.println(grid.getColumns());
     }
+
+
 }
