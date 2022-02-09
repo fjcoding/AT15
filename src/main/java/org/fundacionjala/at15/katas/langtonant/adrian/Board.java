46fcf95948300355348576c1e1d18b61e4713e53
Adrian Oviedo <Adrian.Oviedo@fundacion-jala.org>
2022-02-09 16:33:09 -0400

    Fixed issues from the comments

--- src/main/java/org/fundacionjala/at15/katas/langtonant/adrian/Board.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/adrian/Board.java
@@ -3,34 +3,32 @@ package org.fundacionjala.at15.katas.langtonant.adrian;
 public class Board {
     private int width;
     private int height;
-    private int[][] tablero;
+    private int[][] grid;
 
     public Board(int width, int height) {
         this.width = width;
         this.height = height;
-        tablero = new int[height][width];
-        for (int index = 0; index < tablero.length; index++) {
-            for (int jndex = 0; jndex < tablero[index].length; jndex++) {
-                tablero[index][jndex] = 0;
-                //System.out.print(tablero[i][j]+" ");
+        grid = new int[height][width];
+        for (int index = 0; index < grid.length; index++) {
+            for (int jndex = 0; jndex < grid[index].length; jndex++) {
+                grid[index][jndex] = 0;
             }
-            //System.out.println();
         }
     }
     public void print() {
-        for (int index = 0; index < tablero.length; index++) {
-            for (int jndex = 0; jndex < tablero[index].length; jndex++) {
-                System.out.print(tablero[index][jndex] + " ");
+        for (int index = 0; index < grid.length; index++) {
+            for (int jndex = 0; jndex < grid[index].length; jndex++) {
+                System.out.print(grid[index][jndex] + " ");
             }
             System.out.println();
         }
     }
 
     public int[][] getTablero() {
-        return tablero;
+        return grid;
     }
     public int getPosTablero(int y, int x) {
-        return tablero[y][x];
+        return grid[y][x];
     }
     public int getHeight() {
         return height;
@@ -40,9 +38,9 @@ public class Board {
     }
 
     public void setPos0(int y, int x) {
-        tablero[y][x] = 0;
+        grid[y][x] = 0;
     }
     public void setPos1(int y, int x) {
-        tablero[y][x] = 1;
+        grid[y][x] = 1;
     }
 }
--- src/main/java/org/fundacionjala/at15/katas/langtonant/adrian/MainAdrian.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/adrian/MainAdrian.java
@@ -2,6 +2,6 @@ package org.fundacionjala.at15.katas.langtonant.adrian;
 
 public class MainAdrian {
     public static void main(String[] args) {
-        System.out.println("Ejemplo de Task de Adrian");
+        System.out.println("Adrian's task example");
     }
 }
