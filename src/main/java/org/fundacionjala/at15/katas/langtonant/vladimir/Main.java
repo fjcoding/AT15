c3f2efc14c47006c1a9543d92e1985d375fb03dc
vladimir <vla.evtm@gmail.com>
2022-02-09 13:24:26 -0400

    Error checkstyle [VisibilityModifier] correction in Main.java file, line 6

--- src/main/java/org/fundacionjala/at15/katas/langtonant/vladimir/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/vladimir/Main.java
@@ -3,7 +3,7 @@ package org.fundacionjala.at15.katas.langtonant.vladimir;
 import java.util.*;
 
 public class Main {
-static String[][] mat;
+private static String[][] mat;
 	public static void main(String[] args) {
 			System.out.println("This is Langton's Ant main class, you can do yout implementation here :)");
 			//Scanner scanner=new Scanner(System.in);
