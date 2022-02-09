0dd0be5b59c83925b422f16017713dfe0417229f
Jose Guevara <jose.guevara@fundacion-jala.org>
2022-02-08 23:50:15 -0400

    test

--- src/main/java/org/fundacionjala/at15/katas/langtonant/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/langtonant/Main.java
@@ -5,4 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is Langton's Ant main class, you can do yout implementation here :)");
     }
+
+    public Main() { }
 }
