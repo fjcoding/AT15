d8cf6c10f8899e3f556024e5e116b8829be5eaad
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-02-09 22:50:09 -0300

    removed adhemar & adrian folders

--- src/main/java/org/fundacionjala/at15/katas/langtonant/adrian/MainAdrian.java
+++
@@ -1,7 +0,0 @@
-package org.fundacionjala.at15.katas.langtonant.adrian;
-
-public class MainAdrian {
-    public static void main(String[] args) {
-        System.out.println("Adrian's task example");
-    }
-}
