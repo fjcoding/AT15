34bc831bc5297a900351fe3e17c625a4d81c0f4b
Sergio <Sergio.Mendieta@fundacion-jala.org>
2022-02-11 00:37:49 -0400

    code error in method matrix fixed

--- src/main/java/org/fundacionjala/at15/katas/bankocr/sergio/Spliter.java
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/sergio/Spliter.java
@@ -15,7 +15,7 @@ public class Spliter {
     }
 
     public void theSplit(){
-        lineOneArray = lineOne.split("(?!^");
+        lineOneArray = lineOne.split("(?!^)");
         lineTwoArray = lineTwo.split("(?!^)");
         lineThreeArray = lineThree.split("(?!^)");
     }
@@ -23,50 +23,76 @@ public class Spliter {
     private String [][] matrixDigit(int start){
 
         String [][] resp = new String[3][3];
-        for(int i= start; i<start+3; i++){
-            for(int j = 0; j < 3;j++){
-                resp[j][0] = lineOneArray[i];
-                resp[j][1] = lineTwoArray[i];
-                resp[j][2] = lineThreeArray[i];
-            }
-        }
+        int pos1 = start;
+        int pos2 = start +1;
+        int pos3 = start +2;
+
+        resp[0][0] = lineOneArray[pos1];
+        resp[0][1] = lineOneArray[pos2];
+        resp[0][2] = lineOneArray[pos3];
+
+        resp[1][0] = lineTwoArray[pos1];
+        resp[1][1] = lineTwoArray[pos2];
+        resp[1][2] = lineTwoArray[pos3];
+
+        resp[2][0] = lineThreeArray[pos1];
+        resp[2][1] = lineThreeArray[pos2];
+        resp[2][2] = lineThreeArray[pos3];
+
         return resp;
     }
 
     public String loneDigit(String [][] digit){
         String numberDef;
-        if (digit[0][0]== " " && digit[0][1]== " " && digit[0][2]== " " && digit[1][0]== " " && digit[1][1]== " " && digit[1][2]== "|" && digit[2][0]== " " && digit[2][1]== " " && digit[2][2]== "|"){
+        if(digit[0][0].equals(" ") && digit[0][1].equals(" ") && digit[0][2].equals(" ")
+                && digit[1][0].equals(" ") && digit[1][1].equals(" ") && digit[1][2].equals("|")
+                && digit[2][0].equals(" ") && digit[2][1].equals(" ") && digit[2][2].equals("|")){
             numberDef = "1";
         //return "1";
-        } else if (digit[0][0]== " " && digit[0][1]== "_" && digit[0][2]== " " && digit[1][0]== " " && digit[1][1]== "_" && digit[1][2]== "|" && digit[2][0]== "|" && digit[2][1]== "_" && digit[2][2]== ""){
+        } else if (digit[0][0].equals(" ") && digit[0][1].equals("_") && digit[0][2].equals(" ")
+                && digit[1][0].equals(" ") && digit[1][1].equals("_") && digit[1][2].equals("|")
+                && digit[2][0].equals("|") && digit[2][1].equals("_") && digit[2][2].equals(" ")){
             numberDef = "2";
         //return "2";
-        } else if (digit[0][0]== " " && digit[0][1]== "_" && digit[0][2]== " " && digit[1][0]== " " && digit[1][1]== "_" && digit[1][2]== "|" && digit[2][0]== " " && digit[2][1]== "_" && digit[2][2]== "|"){
+        } else if (digit[0][0].equals(" ") && digit[0][1].equals("_") && digit[0][2].equals(" ")
+                && digit[1][0].equals(" ") && digit[1][1].equals("_") && digit[1][2].equals("|")
+                && digit[2][0].equals(" ") && digit[2][1].equals("_") && digit[2][2].equals("|")){
             numberDef = "3";
         //return "3";
-        } else if (digit[0][0]== " " && digit[0][1]== " " && digit[0][2]== " " && digit[1][0]== "|" && digit[1][1]== "_" && digit[1][2]== "|" && digit[2][0]== " " && digit[2][1]== " " && digit[2][2]== "|"){
+        } else if (digit[0][0].equals(" ") && digit[0][1].equals(" ") && digit[0][2].equals(" ")
+                && digit[1][0].equals("|") && digit[1][1].equals("_") && digit[1][2].equals("|")
+                && digit[2][0].equals(" ") && digit[2][1].equals(" ") && digit[2][2].equals("|")){
             numberDef = "4";
         //return "4";
-        } else if (digit[0][0]== " " && digit[0][1]== "_" && digit[0][2]== " " && digit[1][0]== "|" && digit[1][1]== "_" && digit[1][2]== " " && digit[2][0]== " " && digit[2][1]== "_" && digit[2][2]== "|"){
+        } else if (digit[0][0].equals(" ") && digit[0][1].equals("_") && digit[0][2].equals(" ")
+                && digit[1][0].equals("|") && digit[1][1].equals("_") && digit[1][2].equals(" ")
+                && digit[2][0].equals(" ") && digit[2][1].equals("_") && digit[2][2].equals("|")){
             numberDef = "5";
         //return "5";
-        } else if (digit[0][0]== " " && digit[0][1]== "_" && digit[0][2]== " " && digit[1][0]== "|" && digit[1][1]== "_" && digit[1][2]== " " && digit[2][0]== "|" && digit[2][1]== "_" && digit[2][2]== "|"){
+        } else if (digit[0][0].equals(" ") && digit[0][1].equals("_") && digit[0][2].equals(" ")
+                && digit[1][0].equals("|") && digit[1][1].equals("_") && digit[1][2].equals(" ")
+                && digit[2][0].equals("|") && digit[2][1].equals("_") && digit[2][2].equals("|")){
             numberDef = "6";
         //return "6";
-        } else if (digit[0][0]== " " && digit[0][1]== "_" && digit[0][2]== " " && digit[1][0]== " " && digit[1][1]== " " && digit[1][2]== "|" && digit[2][0]== " " && digit[2][1]== " " && digit[2][2]== "|"){
+        } else if (digit[0][0].equals(" ") && digit[0][1].equals("_") && digit[0][2].equals(" ")
+                && digit[1][0].equals(" ") && digit[1][1].equals(" ") && digit[1][2].equals("|")
+                && digit[2][0].equals(" ") && digit[2][1].equals(" ") && digit[2][2].equals("|")){
             numberDef = "7";
         //return "7";
-        } else if (digit[0][0]== " " && digit[0][1]== "_" && digit[0][2]== " " && digit[1][0]== "|" && digit[1][1]== "_" && digit[1][2]== "|" && digit[2][0]== "|" && digit[2][1]== "_" && digit[2][2]== "|"){
+        } else if (digit[0][0].equals(" ") && digit[0][1].equals("_") && digit[0][2].equals(" ")
+                && digit[1][0].equals("|") && digit[1][1].equals("_") && digit[1][2].equals("|")
+                && digit[2][0].equals("|") && digit[2][1].equals("_") && digit[2][2].equals("|")){
             numberDef = "8";
         //return "8";
-        } else if (digit[0][0]== " " && digit[0][1]== "_" && digit[0][2]== " " && digit[1][0]== "|" && digit[1][1]== "_" && digit[1][2]== "|" && digit[2][0]== " " && digit[2][1]== "_" && digit[2][2]== "|"){
+        } else if (digit[0][0].equals(" ") && digit[0][1].equals("_") && digit[0][2].equals(" ")
+                && digit[1][0].equals("|") && digit[1][1].equals("_") && digit[1][2].equals("|")
+                && digit[2][0].equals(" ") && digit[2][1].equals("_") && digit[2][2].equals("|")){
             numberDef = "9";
         //return "9";
         } else {
             numberDef = "?";
         }
         return numberDef;
-
     }
     public String digitoCompleto(){
         theSplit();
