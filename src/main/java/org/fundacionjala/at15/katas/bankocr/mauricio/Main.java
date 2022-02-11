3a396acfe7294e5aef5b0c76ff878064d5fd683c
Mauricio Davalos Llave <Mauricio.Davalos@fundacion-jala.org>
2022-02-10 23:47:38 -0400

    Add some code for BankOcr new kata

---
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/mauricio/Main.java
@@ -0,0 +1,36 @@
+package org.fundacionjala.at15.katas.bankocr.mauricio;
+public class Main {
+    static final int NUM_0 = 0;
+    static final int NUM_1 = 1;
+    static final int NUM_2 = 2;
+    static final int NUM_3 = 3;
+    static final int NUM_4 = 4;
+    static final int NUM_5 = 5;
+    static final int NUM_6 = 6;
+    static final int NUM_7 = 7;
+    static final int NUM_8 = 8;
+    static final int NUM_9 = 9;
+
+    public static void main(String[] args) {
+        Number num0 = new Number(NUM_0);
+        num0.printNumber();
+        Number num1 = new Number(NUM_1);
+        num1.printNumber();
+        Number num2 = new Number(NUM_2);
+        num2.printNumber();
+        Number num3 = new Number(NUM_3);
+        num3.printNumber();
+        Number num4 = new Number(NUM_4);
+        num4.printNumber();
+        Number num5 = new Number(NUM_5);
+        num5.printNumber();
+        Number num6 = new Number(NUM_6);
+        num6.printNumber();
+        Number num7 = new Number(NUM_7);
+        num7.printNumber();
+        Number num8 = new Number(NUM_8);
+        num8.printNumber();
+        Number num9 = new Number(NUM_9);
+        num9.printNumber();
+    }
+}
