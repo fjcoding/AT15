59ade0c037a4eb55ddcd7e3ebff9e39ec280cff5
Gonzalo Encinas <Gonzalo.Encinas@fundacion-jala.org>
2022-02-13 17:58:54 -0400

    delete TXTread, create txtReading and txtReadingTest

--- src/main/java/org/fundacionjala/at15/katas/bankocr/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/Main.java
@@ -3,8 +3,6 @@ package org.fundacionjala.at15.katas.bankocr;
 public class Main {
 
     public static void main(String[] args) {
-        ReadTXT numberTest= new ReadTXT();
-        numberTest.readTxtFile("enter file location");
-        System.out.print(numberTest);
+        
     }
 }
---
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/txtReadingTest.java
@@ -0,0 +1,5 @@
+package org.fundacionjala.at15.katas.bankocr;
+
+public class txtReadingTest {
+    
+}
