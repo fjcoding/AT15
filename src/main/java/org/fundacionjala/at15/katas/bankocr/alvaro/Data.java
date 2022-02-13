306dcd9e9cfdb8bc0f1d24990da267a6e1ac293b
Alvaro Sivila <Alvaro.Sivila@fundacion-jala.org>
2022-02-13 15:28:23 -0400

    create first datatest

--- src/main/java/org/fundacionjala/at15/katas/bankocr/alvaro/Data.java
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/alvaro/Data.java
@@ -3,9 +3,4 @@ package org.fundacionjala.at15.katas.bankocr.alvaro;
 public class Data
 {
 
-    private char[] zeroLine1;
-    public Data() {
-        int index=0;
-
-    }
 }
---
+++ src/test/java/org/fundacionjala/at15/katas/langtonant/alvarotest/DataTest.java
@@ -0,0 +1,11 @@
+package org.fundacionjala.at15.katas.bankocr.alvaro;
+
+import org.junit.Test;
+
+public class DataTest {
+
+    @Test
+    public void itshouldread4timesthekeyboard(){
+        Data newdata=new Data();
+    }
+}
