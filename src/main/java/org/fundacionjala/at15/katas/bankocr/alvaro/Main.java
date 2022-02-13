e996004178222be3d442ac83a2860abb9c9a1083
Alvaro Sivila <Alvaro.Sivila@fundacion-jala.org>
2022-02-13 18:15:12 -0400

    Added test and functions

--- src/main/java/org/fundacionjala/at15/katas/bankocr/alvaro/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/alvaro/Main.java
@@ -1,9 +1,6 @@
 package org.fundacionjala.at15.katas.bankocr.alvaro;
-import java.util.Scanner;
-public class Main
-{
-        public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
 
     }
 }
