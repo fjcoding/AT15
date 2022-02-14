d63ed4751ff9bd696e0f17a8ea1e8dc71bc1dab8
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-02-14 01:37:43 -0400

    Add digit patterns from 0 to 9 to Digit class

--- src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/Digit.java
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/Digit.java
@@ -2,6 +2,16 @@ package org.fundacionjala.at15.katas.bankocr.adhemar;
 
 public class Digit {
     public static final String PATTERN_0 = " _ \n" + "| |\n" + "|_|\n" + "   \n";
+    public static final String PATTERN_1 = "   \n" + "  |\n" + "  |\n" + "   \n";
+    public static final String PATTERN_2 = " _ \n" + " _|\n" + "|_ \n" + "   \n";
+    public static final String PATTERN_3 = " _ \n" + " _|\n" + " _|\n" + "   \n";
+    public static final String PATTERN_4 = "   \n" + "|_|\n" + "  |\n" + "   \n";
+    public static final String PATTERN_5 = " _ \n" + "|_ \n" + " _|\n" + "   \n";
+    public static final String PATTERN_6 = " _ \n" + "|_ \n" + "|_|\n" + "   \n";
+    public static final String PATTERN_7 = " _ \n" + "  |\n" + "  |\n" + "   \n";
+    public static final String PATTERN_8 = " _ \n" + "|_|\n" + "|_|\n" + "   \n";
+    public static final String PATTERN_9 = " _ \n" + "|_|\n" + " _|\n" + "   \n";
+
     private String content;
 
     Digit(String content) {
