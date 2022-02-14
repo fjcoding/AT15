8398d193f50c9f3617ce5c430f0d44407927f4bb
Gonzalo Encinas <Gonzalo.Encinas@fundacion-jala.org>
2022-02-13 23:49:27 -0400

    modification to test validAccount, the constructor function

--- src/main/java/org/fundacionjala/at15/katas/bankocr/ValidAccount.java
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/ValidAccount.java
@@ -1,29 +1,35 @@
 package org.fundacionjala.at15.katas.bankocr;
 
-/*import java.util.ArrayList;
-import java.util.List;*/
+import java.util.ArrayList;
+import java.util.List;
 
 public class ValidAccount {
 
-    //private List<String> accountlist;
-    private String account;
+    private List<String> accountlist;
     private boolean validation = true;
 
     public ValidAccount(String path) {
         //accountlist = path;
-        account = "345882865";
-    }
-
-    //convert list obtained from String to integer
-    public int convertToInteger() {
-        int accountInt = Integer.parseInt(account);
-        return accountInt;
-    }
-
-    public void accountIsValid() {
+        accountlist = new ArrayList<>();
+        //added for the test
+        accountlist.add("345882865");
+        accountlist.add("000000051");
+        //changed for the test
+        accountlist.add("490067715");
+        //changed for the test
+        accountlist.add("123456789");
     }
 
     public boolean getValidation() {
         return validation;
+    }
+
+    public boolean isItInteger() {
+        boolean isInteger = true;
+        return isInteger;
+    }
+
+    public List<String> getAccountsToValidate() {
+        return accountlist;
     }
 }
--- src/test/java/org/fundacionjala/at15/katas/banckorc/validAccountTest.java
+++ src/test/java/org/fundacionjala/at15/katas/banckorc/validAccountTest.java
@@ -1,6 +1,10 @@
 package org.fundacionjala.at15.katas.banckorc;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.fundacionjala.at15.katas.bankocr.ValidAccount;
 import org.junit.Test;
 
@@ -12,5 +16,25 @@ public class validAccountTest {
         ValidAccount vAccount = new ValidAccount("D:\\workspace\\coding\\AT15\\src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\txtFile\\validAccount.txt");
         assertEquals(valid, vAccount.getValidation());
     }
-    
+
+    @Test
+    public void shouldBeAccountsToValidate() {
+        List<String> exp = new ArrayList<>();
+        //added for the test
+        exp.add("345882865");
+        exp.add("000000051");
+        //changed for the test
+        exp.add("490067715");
+        //changed for the test
+        exp.add("123456789");
+        ValidAccount vAccount = new ValidAccount("D:\\workspace\\coding\\AT15\\src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\txtFile\\validAccount.txt");
+        assertEquals(exp, vAccount.getAccountsToValidate());
+    }
+
+    @Test
+    public void itShouldBeIntegers() {
+        boolean valid = true;
+        ValidAccount vAccount = new ValidAccount("D:\\workspace\\coding\\AT15\\src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\txtFile\\validAccount.txt");
+        assertEquals(valid, vAccount.getValidation());
+    }
 }
