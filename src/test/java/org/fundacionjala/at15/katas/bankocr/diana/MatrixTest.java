285593e058046cf2b82a5d900fe7c9e8570de647
Diana Rojas <diana.rojas@fundacion-jala.org>
2022-02-14 19:23:26 -0400

    Unit test of numbers in matrixes 4x3

--- src/test/java/org/fundacionjala/at15/katas/bankocr/diana/MatrixTest.java
+++ src/test/java/org/fundacionjala/at15/katas/bankocr/diana/MatrixTest.java
@@ -14,6 +14,69 @@ public class MatrixTest {
         String matrixNumber[][] = new String[4][3];
         String matrixExpected[][] = { { " ", "_", " " }, { "|", " ", "|" }, { "|", "_", "|" }, { " ", " ", " " } };
         assertArrayEquals(matrixExpected, testNumber.numberZero(matrixNumber));
-        // assertArrayEquals(matrixExpected, matrixNumber.numberZero());
     }
+
+    @Test
+    public void IsMatrixReturningOne() {
+        String matrixNumber[][] = new String[4][3];
+        String matrixExpected[][] = { { " ", " ", " " }, { " ", " ", "|" }, { " ", " ", "|" }, { " ", " ", " " } };
+        assertArrayEquals(matrixExpected, testNumber.numberOne(matrixNumber));
+    }
+
+    @Test
+    public void IsMatrixReturningTwo() {
+        String matrixNumber[][] = new String[4][3];
+        String matrixExpected[][] = { { " ", "_", " " }, { " ", "_", "|" }, { "|", "_", " " }, { " ", " ", " " } };
+        assertArrayEquals(matrixExpected, testNumber.numberTwo(matrixNumber));
+    }
+
+    @Test
+    public void IsMatrixReturningThree() {
+        String matrixNumber[][] = new String[4][3];
+        String matrixExpected[][] = { { " ", "_", " " }, { " ", "_", "|" }, { " ", "_", "|" }, { " ", " ", " " } };
+        assertArrayEquals(matrixExpected, testNumber.numberThree(matrixNumber));
+    }
+
+    @Test
+    public void IsMatrixReturningFour() {
+        String matrixNumber[][] = new String[4][3];
+        String matrixExpected[][] = { { " ", " ", " " }, { "|", "_", "|" }, { " ", " ", "|" }, { " ", " ", " " } };
+        assertArrayEquals(matrixExpected, testNumber.numberFour(matrixNumber));
+    }
+
+    @Test
+    public void IsMatrixReturningFve() {
+        String matrixNumber[][] = new String[4][3];
+        String matrixExpected[][] = { { " ", "_", " " }, { "|", "_", " " }, { " ", "_", "|" }, { " ", " ", " " } };
+        assertArrayEquals(matrixExpected, testNumber.numberFive(matrixNumber));
+    }
+
+    @Test
+    public void IsMatrixReturningSix() {
+        String matrixNumber[][] = new String[4][3];
+        String matrixExpected[][] = { { " ", "_", " " }, { "|", "_", " " }, { "|", "_", "|" }, { " ", " ", " " } };
+        assertArrayEquals(matrixExpected, testNumber.numberSix(matrixNumber));
+    }
+
+    @Test
+    public void IsMatrixReturningSeven() {
+        String matrixNumber[][] = new String[4][3];
+        String matrixExpected[][] = { { " ", "_", " " }, { " ", " ", "|" }, { " ", " ", "|" }, { " ", " ", " " } };
+        assertArrayEquals(matrixExpected, testNumber.numberSeven(matrixNumber));
+    }
+
+    @Test
+    public void IsMatrixReturningEight() {
+        String matrixNumber[][] = new String[4][3];
+        String matrixExpected[][] = { { " ", "_", " " }, { "|", "_", "|" }, { "|", "_", "|" }, { " ", " ", " " } };
+        assertArrayEquals(matrixExpected, testNumber.numberEight(matrixNumber));
+    }
+
+    @Test
+    public void IsMatrixReturningNine() {
+        String matrixNumber[][] = new String[4][3];
+        String matrixExpected[][] = { { " ", "_", " " }, { "|", "_", "|" }, { " ", "_", "|" }, { " ", " ", " " } };
+        assertArrayEquals(matrixExpected, testNumber.numberNine(matrixNumber));
+    }
+
 }
