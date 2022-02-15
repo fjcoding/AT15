044bc83fd503c2a2f16538c8893b6ca8f3fc1ac5
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-02-15 08:31:29 -0400

    Fix all paths from \ to /

--- src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/Main.java
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/Main.java
@@ -2,7 +2,7 @@ package org.fundacionjala.at15.katas.bankocr.adhemar;
 
 public class Main {
     public static void main(String[] args) {
-        IOFile theFile = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\ThreeEntryFile.txt");
+        IOFile theFile = new IOFile("src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/files/ThreeEntryFile.txt");
         System.out.println("Input File:\n" + theFile.getStringContent());
         System.out.println("Account Numbers:\n" + theFile.getAccountNumbers());
     }
--- src/test/java/org/fundacionjala/at15/katas/bankocr/adhemar/IOFileTest.java
+++ src/test/java/org/fundacionjala/at15/katas/bankocr/adhemar/IOFileTest.java
@@ -7,23 +7,17 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class IOFileTest {
-    @Test
-    public void itShouldHaveAPath() {
-        IOFile theFile = new IOFile("files\\testFile.txt");
-
-        assertEquals("files\\testFile.txt", theFile.getPath());
-    }
 
     @Test
     public void itShouldReadAOneLineFile() {
-        IOFile theFile = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\HelloTestFile.txt");
+        IOFile theFile = new IOFile("src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/files/HelloTestFile.txt");
 
         assertEquals("Hello\n", theFile.getStringContent());
     }
 
     @Test
     public void itShouldReadMultiLinesFiles() {
-        IOFile theFile = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\OneEntryFile.txt");
+        IOFile theFile = new IOFile("src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/files/OneEntryFile.txt");
         String multiLineFile =
         "    _  _     _  _  _  _  _ \n" +
         "  | _| _||_||_ |_   ||_||_|\n" +
@@ -35,25 +29,25 @@ public class IOFileTest {
 
     @Test
     public void itShouldVerifyCorrectNumberOfLinesAndCharacteresInTheFile() {
-        IOFile theFile1 = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\ThreeEntryFile.txt");
+        IOFile theFile1 = new IOFile("src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/files/ThreeEntryFile.txt");
 
         assertTrue(theFile1.verifyFile());
 
-        IOFile theFile2 = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\ErrorFile.txt");
+        IOFile theFile2 = new IOFile("src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/files/ErrorFile.txt");
 
         assertFalse(theFile2.verifyFile());
     }
 
     @Test
     public void itShouldSplitEntriesOfTheFile() {
-        IOFile theFile = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\ThreeEntryFile.txt");
+        IOFile theFile = new IOFile("src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/files/ThreeEntryFile.txt");
 
         assertEquals( 3, theFile.getEntries().length);
     }
 
     @Test
     public void itShouldGetAccountNumbersFile() {
-        IOFile theFile1 = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\ThreeEntryFile.txt");
+        IOFile theFile1 = new IOFile("src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/files/ThreeEntryFile.txt");
         String AccountNumbers1 =
         "123456789\n" +
         "000000000\n" +
@@ -61,7 +55,7 @@ public class IOFileTest {
 
         assertEquals(AccountNumbers1, theFile1.getAccountNumbers());
 
-        IOFile theFile2 = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\OneEntryFile.txt");
+        IOFile theFile2 = new IOFile("src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/files/OneEntryFile.txt");
         String AccountNumbers2 = "123456789\n";
 
         assertEquals(AccountNumbers2, theFile2.getAccountNumbers());
