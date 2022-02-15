83c62cafb58b6d288e6768bfd68c0fee01456a76
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-02-14 23:35:22 -0400

    Add an empty digit patterns array to Entry class then get digit patterns

--- src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/Entry.java
+++ src/main/java/org/fundacionjala/at15/katas/bankocr/adhemar/Entry.java
@@ -2,12 +2,39 @@ package org.fundacionjala.at15.katas.bankocr.adhemar;
 
 public class Entry {
     private String origin;
+    private String[] digitPatterns;
+    private static final int DIGITS_LINE = 9;
+    private static final int CHARS_DIGIT = 3;
 
     public Entry(String origin) {
         this.origin = origin;
+        this.digitPatterns = new String[DIGITS_LINE];
+        for (int index = 0; index < this.digitPatterns.length; index++) {
+            this.digitPatterns[index] = "";
+        }
     }
 
     public String getOrigin() {
         return this.origin;
+    }
+
+    public String[] getDigitPatterns() {
+        return this.digitPatterns;
+    }
+
+    public void splitDigitPatterns() {
+        char[] chars = this.origin.toCharArray();
+        int index2 = 0;
+        for (int index1 = 0; index1 < chars.length; index1++) {
+            this.digitPatterns[index2] += String.valueOf(chars[index1]);
+            if ((index1 + 1) % CHARS_DIGIT == 0) {
+                if (index2 + 1 == DIGITS_LINE) {
+                    index2 = 0;
+                } else {
+                    this.digitPatterns[index2] += "\n";
+                    index2++;
+                }
+            }
+        }
     }
 }
--- src/test/java/org/fundacionjala/at15/katas/bankocr/adhemar/EntryTest.java
+++ src/test/java/org/fundacionjala/at15/katas/bankocr/adhemar/EntryTest.java
@@ -14,5 +14,29 @@ public class EntryTest {
 
         assertEquals(Entries[0], theEntry.getOrigin());
     }
-    
+
+    @Test
+    public void itShouldHaveDigitPatternsArrayEmpty() {
+        IOFile theFile = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\test3.txt");
+        theFile.readFile();
+        String[] Entries = theFile.getEntries();
+        Entry theEntry = new Entry(Entries[0]);
+
+        assertEquals("", theEntry.getDigitPatterns()[0]);
+    }
+
+    @Test
+    public void itShouldGetIndividualDigitPatterns() {
+        IOFile theFile = new IOFile("src\\main\\java\\org\\fundacionjala\\at15\\katas\\bankocr\\adhemar\\files\\test3.txt");
+        theFile.readFile();
+        String[] Entries = theFile.getEntries();
+        Entry theEntry = new Entry(Entries[1]);
+        String pattern =
+            " _ \n" +
+            "| |\n" +
+            "|_|\n";
+        theEntry.splitDigitPatterns();
+
+        assertEquals(pattern, theEntry.getDigitPatterns()[0]);
+    }
 }
