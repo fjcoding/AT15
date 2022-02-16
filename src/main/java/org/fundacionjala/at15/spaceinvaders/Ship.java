4045b483092858ea06c256604bb3f8886559d213
Adrian Oviedo <Adrian.Oviedo@fundacion-jala.org>
2022-02-16 19:34:53 -0400

    Added movement of the ship

--- src/main/java/org/fundacionjala/at15/spaceinvaders/Ship.java
+++ src/main/java/org/fundacionjala/at15/spaceinvaders/Ship.java
@@ -1,14 +1,96 @@
 package org.fundacionjala.at15.spaceinvaders;
 
-public class Ship {
-    private int life;
-    private int posX;
-    private int posY;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-    public Ship(int life, int posX, int posY) {
+public class Ship extends JComponent {
+    private int life;
+    private float posX;
+    private float posY;
+    private static final int WIDTH = 500;
+    private static final int HEIGHT = 400;
+    private static final int DIAMETER = 15;
+    private static final int VELOCITYRIGHT = 300;
+    private static final int VELOCITYLEFT = -300;
+    private static final float TIMEINTERNVAL = 1000000000f;
+    private float velocityX;
+    private boolean left;
+    private boolean right;
+
+    public Ship(int life, float posX, float posY) {
         this.life = life;
         this.posX = posX;
         this.posY = posY;
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        addKeyListener(new KeyAdapter() {
+                public void keyPressed(KeyEvent e) {
+                    actualiza(e.getKeyCode(), true);
+                }
+
+                public void keyReleased(KeyEvent e) {
+                    actualiza(e.getKeyCode(), false);
+                }
+
+                private void actualiza(int keyCode, boolean pressed) {
+                    switch (keyCode) {
+                        case KeyEvent.VK_LEFT:
+                            left = pressed;
+                            break;
+                        case KeyEvent.VK_RIGHT:
+                            right = pressed;
+                            break;
+                        default:
+                            System.err.println("Is not right or left");
+                    }
+                }
+            });
+        setFocusable(true);
+    }
+
+    private float moveUptoLimit(float value, float min, float max) {
+        if (value > max) {
+            return max;
+        }
+        if (value < min) {
+            return min;
+        }
+        return value;
+    }
+
+    private void movement(float deltaT) {
+        velocityX = 0;
+        if (left) {
+            velocityX = VELOCITYLEFT;
+        }
+        if (right) {
+            velocityX = VELOCITYRIGHT;
+        }
+        posX = moveUptoLimit(posX + velocityX * deltaT, 0, WIDTH - DIAMETER);
+    }
+
+    public void paint(Graphics graphic) {
+        graphic.setColor(Color.BLACK);
+        graphic.fillRect(0, 0, WIDTH, HEIGHT);
+        graphic.setColor(Color.GREEN);
+        graphic.fillOval(Math.round(posX), Math.round(posY), DIAMETER, DIAMETER);
+    }
+    private void draw() throws Exception {
+        SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    paintImmediately(0, 0, WIDTH, HEIGHT);
+                }
+            });
+    }
+    public void mainCycle() throws Exception {
+        long oldTime = System.nanoTime();
+        while (true) {
+            long newTime = System.nanoTime();
+            float deltaT = (newTime - oldTime) / TIMEINTERNVAL;
+            oldTime = newTime;
+            movement(deltaT);
+            draw();
+        }
     }
 
     public int getLife() {
@@ -17,16 +99,34 @@ public class Ship {
     public void setLife(int parameter) {
         this.life = parameter;
     }
-    public int getPosX() {
+    public float getPosX() {
         return posX;
     }
-    public void setPosX(int parameter) {
+    public void setPosX(float parameter) {
         this.posX = parameter;
     }
-    public int getPosY() {
+    public float getPosY() {
         return posY;
     }
-    public void setPosY(int parameter) {
+    public void setPosY(float parameter) {
         this.posY = parameter;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int life = 1;
+        final float xpos = 250;
+        final float ypos = 385;
+        JFrame jframe = new JFrame("PingBall");
+        jframe.addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);
+                }
+            });
+        jframe.setResizable(false);
+        Ship ship = new Ship(life, xpos, ypos);
+        jframe.getContentPane().add(ship);
+        jframe.pack();
+        jframe.setVisible(true);
+        ship.mainCycle();
     }
 }
--- src/test/java/org/fundacionjala/at15/spaceinvaders/ShipTest.java
+++ src/test/java/org/fundacionjala/at15/spaceinvaders/ShipTest.java
@@ -8,13 +8,13 @@ public class ShipTest {
 
     @Test
     public void itShouldHaveAPositionAndLife() {
-        int positionX = 20;
-        int positionY = 20;
+        float positionX = 20;
+        float positionY = 20;
         int life = 1;
         Ship theShip = new Ship(life, positionX, positionY);
 
         assertEquals(life, theShip.getLife());
-        assertEquals(positionX, theShip.getPosX());
-        assertEquals(positionY, theShip.getPosY());
+        assertEquals(positionX, theShip.getPosX(), 0);
+        assertEquals(positionY, theShip.getPosY(), 0);
     }
 }
