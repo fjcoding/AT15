8395b4ef97e9e0c216965846fd003dbe56223e3a
Adrian Oviedo <Adrian.Oviedo@fundacion-jala.org>
2022-02-16 08:56:41 -0400

    added first test of Ship class

--- src/main/java/org/fundacionjala/at15/spaceinvaders/Ship.java
+++ src/main/java/org/fundacionjala/at15/spaceinvaders/Ship.java
@@ -5,8 +5,28 @@ public class Ship {
     private int posX;
     private int posY;
 
-    public Ship(int posX, int posY) {
+    public Ship(int life, int posX, int posY) {
+        this.life = life;
         this.posX = posX;
         this.posY = posY;
+    }
+
+    public int getLife() {
+        return life;
+    }
+    public void setLife(int parameter) {
+        this.life = parameter;
+    }
+    public int getPosX() {
+        return posX;
+    }
+    public void setPosX(int parameter) {
+        this.posX = parameter;
+    }
+    public int getPosY() {
+        return posY;
+    }
+    public void setPosY(int parameter) {
+        this.posY = parameter;
     }
 }
--- src/test/java/org/fundacionjala/at15/spaceinvaders/ShipTest.java
+++ src/test/java/org/fundacionjala/at15/spaceinvaders/ShipTest.java
@@ -1,5 +1,20 @@
 package org.fundacionjala.at15.spaceinvaders;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class ShipTest {
 
+    @Test
+    public void itShouldHaveAPositionAndLife() {
+        int positionX = 20;
+        int positionY = 20;
+        int life = 1;
+        Ship theShip = new Ship(life, positionX, positionY);
+
+        assertEquals(life, theShip.getLife());
+        assertEquals(positionX, theShip.getPosX());
+        assertEquals(positionY, theShip.getPosY());
+    }
 }
