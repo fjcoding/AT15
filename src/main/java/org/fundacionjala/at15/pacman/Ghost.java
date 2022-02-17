51d2131c1079b8799d444010625b78df7411a9e8
Sergio <Sergio.Mendieta@fundacion-jala.org>
2022-02-16 21:11:54 -0400

    Ghost Class and GhostTest class added

---
+++ src/main/java/org/fundacionjala/at15/pacman/Ghost.java
@@ -0,0 +1,49 @@
+package org.fundacionjala.at15.pacman;
+
+public class Ghost {
+    private int xPos;
+    private int yPos;
+
+    public Ghost(int currentXPos, int currentYPos) {
+        this.xPos = currentXPos;
+        this.yPos = currentYPos;
+    }
+
+    public int getXPos() {
+        return xPos;
+    }
+
+    public int getYPos() {
+        return yPos;
+    }
+
+    public void movementUp() {
+        yPos = yPos - 1;
+    }
+
+    public void movementRight() {
+        xPos = xPos + 1;
+    }
+
+    public void movementDown() {
+        yPos = yPos + 1;
+    }
+
+    public void movementLeft() {
+        xPos = xPos - 1;
+    }
+
+    // public void ghostMoveUp(){
+
+    // DirectionEnum random = DirectionEnum.getDirectionEnum();
+    // if(random.equals(DirectionEnum.UP)){
+    // this.yPos--;
+    // } else if(random.equals(DirectionEnum.RIGHT)){
+    // this.xPos++;
+    // } else if(random.equals(DirectionEnum.DOWN)){
+    // this.yPos++;
+    // } else if(random.equals(DirectionEnum.LEFT)){
+    // this.xPos--;
+    // }
+    // }
+}
