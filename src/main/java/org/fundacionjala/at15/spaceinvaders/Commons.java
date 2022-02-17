4371d20e5993fc06476c2d28f978539babc6347c
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-02-17 16:02:34 -0300

    added commons.java

--- src/main/java/org/fundacionjala/at15/spaceinvaders/Commons.java
+++ src/main/java/org/fundacionjala/at15/spaceinvaders/Commons.java
@@ -1,2 +1,20 @@
-package org.fundacionjala.at15.spaceinvaders;public class Commons {
+package org.fundacionjala.at15.spaceinvaders;
+
+public interface Commons {
+
+    int BOARD_WIDTH = 358;
+    int BOARD_HEIGHT = 350;
+    int BORDER_RIGHT = 30;
+    int BORDER_LEFT = 5;
+    int GROUND = 290;
+
+    int ALIEN_HEIGHT = 12;
+    int ALIEN_WIDTH = 12;
+    int ALIEN_INIT_X = 150;
+    int ALIEN_INIT_Y = 5;
+
+    int PLAYER_WIDTH = 15;
+    int PLAYER_HEIGHT = 10;
+    int START_X = 270;
+    int START_Y = 280;
 }
