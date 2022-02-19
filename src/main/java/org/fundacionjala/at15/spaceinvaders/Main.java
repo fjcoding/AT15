b442508d9dec57d171c8f9f1c15a2771d7c96dd2
Adrian Oviedo <Adrian.Oviedo@fundacion-jala.org>
2022-02-19 08:08:44 -0400

    fixed checkstyle

--- src/main/java/org/fundacionjala/at15/spaceinvaders/Main.java
+++ src/main/java/org/fundacionjala/at15/spaceinvaders/Main.java
@@ -5,10 +5,12 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         int shipLife = 1;
+        final float posX = 250;
+        final float posY = 385;
         JFrame jframe = new JFrame("Space Invaders");
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jframe.setResizable(false);
-        Ship ship = new Ship(shipLife, 250, 385);
+        Ship ship = new Ship(shipLife, posX, posY);
         jframe.getContentPane().add(ship);
         jframe.pack();
         jframe.setVisible(true);
