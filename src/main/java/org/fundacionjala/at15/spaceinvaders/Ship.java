c0f4ae2181efae4ff2267f45339a6c3ff24fc9fa
Adrian Oviedo <Adrian.Oviedo@fundacion-jala.org>
2022-02-19 12:01:37 -0400

    fixed Board class and paint method

--- src/main/java/org/fundacionjala/at15/spaceinvaders/Ship.java
+++ src/main/java/org/fundacionjala/at15/spaceinvaders/Ship.java
@@ -1,55 +1,21 @@
 package org.fundacionjala.at15.spaceinvaders;
 
 import java.awt.event.KeyEvent;
-import javax.swing.*;
-<<<<<<< HEAD
-import org.fundacionjala.at15.spaceinvaders.Constants.*;
-=======
 import java.awt.*;
->>>>>>> dev/spaceinvaders
 
 import static org.fundacionjala.at15.spaceinvaders.Constants.Player.*;
 import static org.fundacionjala.at15.spaceinvaders.Constants.Board.*;
 
-<<<<<<< HEAD
-    public Ship(int life, float posX, float posY) {
-        this.life = life;
-        this.posX = posX;
-        this.posY = posY;
-        setPreferredSize(new Dimension(Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT));
-        addKeyListener(new KeyAdapter() {
-                public void keyPressed(KeyEvent e) {
-                    updateKeyPressed(e.getKeyCode(), true);
-                }
-=======
 public class Ship {
     private int posX = START_X;
     private int posY = START_Y;
->>>>>>> dev/spaceinvaders
 
     public void paint(Graphics graphic) {
         //ImageIcon ship = new ImageIcon(getClass().getResource("src/main/resources/spaceinvaders/ship.png"));
         //graphic.drawImage(ship.getImage(), posX, posY, PLAYER_WIDTH, PLAYER_HEIGHT, null);
 
-<<<<<<< HEAD
-                public void updateKeyPressed(int keyCode, boolean pressed) {
-                    switch (keyCode) {
-                        case KeyEvent.VK_LEFT:
-                            left = pressed;
-                            break;
-                        case KeyEvent.VK_RIGHT:
-                            right = pressed;
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            });
-        setFocusable(true);
-=======
         graphic.setColor(Color.GREEN);
         graphic.fillOval(Math.round(posX), Math.round(posY), DIAMETER, DIAMETER);
->>>>>>> dev/spaceinvaders
     }
 
     public void keyPressed(KeyEvent e) {
@@ -65,11 +31,7 @@ public class Ship {
         }
     }
 
-<<<<<<< HEAD
-    public float moveUptoLimit(float value, float min, float max) {
-=======
     public int moveUptoLimit(int value, int min, int max) {
->>>>>>> dev/spaceinvaders
         if (value > max) {
             return max;
         }
@@ -78,32 +40,6 @@ public class Ship {
         }
         return value;
     }
-<<<<<<< HEAD
-    public void paint(Graphics graphic) {
-        graphic.setColor(Color.BLACK);
-        graphic.fillRect(0, 0, Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
-        graphic.setColor(Color.RED);
-        graphic.fillRect(Commons.ALIEN_INIT_X, Commons.ALIEN_INIT_Y, Commons.ALIEN_WIDTH, Commons.ALIEN_HEIGHT);
-        graphic.setColor(Color.GREEN);
-        graphic.fillOval(Math.round(posX), Math.round(posY), Commons.DIAMETER, Commons.DIAMETER);
-    }
-
-    public void render() throws Exception {
-        SwingUtilities.invokeAndWait(new Runnable() {
-                public void run() {
-                    paintImmediately(0, 0, Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
-                }
-            });
-    }
-
-    public int getLife() {
-        return life;
-    }
-    public void setLife(int parameter) {
-        this.life = parameter;
-    }
-    public float getPosX() {
-=======
 
     /*public Ellipse2D getBoundsBicho(){
         return new Ellipse2D.Double(posX + 10, posY + 30, 80, 50) ;
@@ -116,7 +52,6 @@ public class Ship {
     }*/
 
     public int getPosX() {
->>>>>>> dev/spaceinvaders
         return posX;
     }
     public void setPosX(int parameter) {
@@ -128,11 +63,4 @@ public class Ship {
     public void setPosY(int parameter) {
         this.posY = parameter;
     }
-    public void setLeft(boolean parameter) {
-        this.left = parameter;
-    }
-    public void setRight(boolean parameter) {
-        this.right = parameter;
-    }
-
 }
--- src/test/java/org/fundacionjala/at15/spaceinvaders/ShipTest.java
+++ src/test/java/org/fundacionjala/at15/spaceinvaders/ShipTest.java
@@ -5,80 +5,12 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ShipTest {
-<<<<<<< HEAD
-/*
-    @Test
-=======
-
     /*@Test
->>>>>>> dev/spaceinvaders
     public void itShouldHaveAPositionAndLife() {
         int life = 1;
         Ship theShip = new Ship(life);
-
         assertEquals(life, theShip.getLife());
-<<<<<<< HEAD
         assertEquals(Commons.START_X, theShip.getPosX(), 0);
         assertEquals(Commons.START_Y, theShip.getPosY(), 0);
-    }
-
-    @Test
-    public void itShouldSetAPositionAndLife() {
-        float positionX = 20;
-        float positionY = 20;
-        int life = 1;
-        Ship theShip = new Ship(life, positionX, positionY);
-        theShip.setLife(7);
-        int randomLifeValue = 7;
-        theShip.setPosX(70);
-        int randomPosXValue = 70;
-        theShip.setPosY(80);
-        int randomPosYValue = 80;
-
-        assertEquals(randomLifeValue, theShip.getLife());
-        assertEquals(randomPosXValue, theShip.getPosX(), 0);
-        assertEquals(randomPosYValue, theShip.getPosY(), 0);
-    }
-
-    @Test
-    public void itShouldReturnMaxAndMinValue() {
-        float positionX = 20;
-        float positionY = 20;
-        int life = 1;
-        Ship theShip = new Ship(life, positionX, positionY);
-        float min = 0;
-        float max = 1000;
-        float value = 1001;
-        assertEquals(max, theShip.moveUptoLimit(value, min, max), 0);
-        value = -1;
-        assertEquals(min, theShip.moveUptoLimit(value, min, max), 0);
-        value = 500;
-        assertEquals(500, theShip.moveUptoLimit(value, min, max), 0);
-    }
-
-    @Test
-    public void itShouldMove() {
-        float positionX = 100;
-        float positionY = 100;
-        int life = 1;
-        long oldTime = System.nanoTime();
-        long newTime = System.nanoTime();
-        float deltaTime = (newTime - oldTime) / 1000000000f;
-        Ship theShip = new Ship(life, positionX, positionY);
-
-        theShip.movement(deltaTime);
-        assertEquals(101, theShip.getPosX(), 1000000000f);
-
-        theShip.setLeft(true);
-        theShip.movement(deltaTime);
-        assertEquals(100, theShip.getPosX(), 1000000000f);
-
-        theShip.setRight(true);
-        theShip.movement(deltaTime);
-        assertEquals(101, theShip.getPosX(), 1000000000f);
-=======
-        assertEquals(START_X, theShip.getPosX(), 0);
-        assertEquals(START_Y, theShip.getPosY(), 0);
->>>>>>> dev/spaceinvaders
-    }*/
+    } */
 }
