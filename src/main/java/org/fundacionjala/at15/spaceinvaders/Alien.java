a870de84d366853635c972d5d6935095289825eb
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-02-19 18:01:10 -0400

    Aliens can move from right to left, Add render functionality to Conteiner class

--- src/main/java/org/fundacionjala/at15/spaceinvaders/Alien.java
+++ src/main/java/org/fundacionjala/at15/spaceinvaders/Alien.java
@@ -1,10 +1,13 @@
 package org.fundacionjala.at15.spaceinvaders;
 
+import java.awt.*;
+
 public class Alien {
     private int posX;
     private int posY;
     private int width;
     private int height;
+    private int deltaX = 1;
 
     public Alien(int posX, int posY, int width, int height) {
         this.posX = posX;
@@ -27,5 +30,22 @@ public class Alien {
 
     public int getHeight() {
         return this.height;
+    }
+
+    public int getDeltaX() {
+        return this.deltaX;
+    }
+
+    public void setDeltaX(int deltaXVar) {
+        this.deltaX = deltaXVar;
+    }
+
+    public void moveX() {
+        this.posX += this.deltaX;
+    }
+
+    public void paint(Graphics graphics) {
+        graphics.setColor(Color.RED);
+        graphics.fillOval(this.getPosX(), this.getPosY(), this.getWidth(), this.getHeight());
     }
 }
---
+++ src/main/java/org/fundacionjala/at15/spaceinvaders/Commons.java
@@ -0,0 +1,27 @@
+package org.fundacionjala.at15.spaceinvaders;
+
+public interface Commons {
+    int BOARD_WIDTH = 800;
+    int BOARD_HEIGHT = 600;
+
+    int ALIEN_INIT_X = 10;
+    int ALIEN_INIT_Y = 10;
+    int ALIEN_HEIGHT = 10;
+    int ALIEN_WIDTH = 20;
+    int ALIEN_ROWS = 5;
+    int ALIEN_COLUMNS = 12;
+    int ALIEN_SEPARATION = 35;
+
+    int BORDER_RIGHT = 30;
+    int BORDER_LEFT = 5;
+    int GROUND = 290;
+
+    int DIAMETER = 15;
+
+    int PLAYER_WIDTH = 15;
+    int PLAYER_HEIGHT = 10;
+    int START_X = 250;
+    int START_Y = 385;
+
+    int MILLIS = 10;
+}
