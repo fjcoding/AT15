282468e66c55db5f641888c7d22f3e101ccecbde
Adrian Oviedo <Adrian.Oviedo@fundacion-jala.org>
2022-02-22 18:53:06 -0400

    deleted spaces

--- src/main/java/org/fundacionjala/at15/spaceinvaders/Constants.java
+++ src/main/java/org/fundacionjala/at15/spaceinvaders/Constants.java
@@ -3,7 +3,6 @@ package org.fundacionjala.at15.spaceinvaders;
 public final class Constants {
 
     public final class Board {
-
         static final int BOARD_WIDTH = 800;
         static final int BOARD_HEIGHT = 600;
         static final int GROUND = 290;
@@ -11,7 +10,6 @@ public final class Constants {
     }
 
     public final class Player {
-
         static final int PLAYER_WIDTH = 15;
         static final int PLAYER_HEIGHT = 10;
         static final int START_X = 400;
@@ -21,7 +19,6 @@ public final class Constants {
     }
 
     public final class Alien {
-
         static final int ALIEN_INIT_X = 10;
         static final int ALIEN_INIT_Y = 10;
         static final int ALIEN_HEIGHT = 10;
