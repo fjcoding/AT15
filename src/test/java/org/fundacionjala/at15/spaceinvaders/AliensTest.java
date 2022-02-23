25f919aa53a4a784a0ef815e7f84b27cd9d3b1bb
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-02-22 21:16:47 -0400

    Add Aliens test, it should move left down

--- src/test/java/org/fundacionjala/at15/spaceinvaders/AliensTest.java
+++ src/test/java/org/fundacionjala/at15/spaceinvaders/AliensTest.java
@@ -2,8 +2,6 @@ package org.fundacionjala.at15.spaceinvaders;
 import static org.fundacionjala.at15.spaceinvaders.Constants.Alien.*;
 import static org.junit.Assert.assertEquals;
 
-import java.util.*;
-
 import org.junit.Test;
 
 public class AliensTest {
@@ -19,11 +17,11 @@ public class AliensTest {
         Aliens theAliens = new Aliens(ALIEN_ROWS, ALIEN_COLUMNS);
         int[] newPosX = new int[ALIEN_ROWS * ALIEN_COLUMNS];
         for (int index = 0; index < ALIEN_ROWS * ALIEN_COLUMNS; index++) {
-            newPosX[index] = theAliens.getAliens().get(0).getPosX();
+            newPosX[index] = theAliens.getAliens().get(index).getPosX();
         }
         theAliens.moveAliens();
         for (int index = 0; index < ALIEN_ROWS * ALIEN_COLUMNS; index++) {
-            assertEquals(newPosX[index] + ALIEN_DELTA_X, theAliens.getAliens().get(0).getPosX());
+            assertEquals(newPosX[index] + ALIEN_DELTA_X, theAliens.getAliens().get(index).getPosX());
         }
     }
 
@@ -32,12 +30,29 @@ public class AliensTest {
         Aliens theAliens = new Aliens(ALIEN_ROWS, ALIEN_COLUMNS);
         int[] newPosX = new int[ALIEN_ROWS * ALIEN_COLUMNS];
         for (int index = 0; index < ALIEN_ROWS * ALIEN_COLUMNS; index++) {
-            newPosX[index] = theAliens.getAliens().get(0).getPosX();
+            newPosX[index] = theAliens.getAliens().get(index).getPosX();
         }
         theAliens.setAlienDeltaX(-ALIEN_DELTA_X);
         theAliens.moveAliens();
         for (int index = 0; index < ALIEN_ROWS * ALIEN_COLUMNS; index++) {
-            assertEquals(newPosX[index] - ALIEN_DELTA_X, theAliens.getAliens().get(0).getPosX());
+            assertEquals(newPosX[index] - ALIEN_DELTA_X, theAliens.getAliens().get(index).getPosX());
+        }
+    }
+
+    @Test
+    public void itShouldMoveLeftDown() {
+        Aliens theAliens = new Aliens(ALIEN_ROWS, ALIEN_COLUMNS);
+        int[] newPosY = new int[ALIEN_ROWS * ALIEN_COLUMNS];
+        for (Alien alien : theAliens.getAliens()) {
+            alien.setPosX(alien.getPosX() - ALIEN_INIT_X);
+        }
+        for (int index = 0; index < ALIEN_ROWS * ALIEN_COLUMNS; index++) {
+            newPosY[index] = theAliens.getAliens().get(index).getPosY();
+        }
+        theAliens.setAlienDeltaX(-ALIEN_DELTA_X);
+        theAliens.moveAliens();
+        for (int index = 0; index < ALIEN_ROWS * ALIEN_COLUMNS; index++) {
+            assertEquals(newPosY[index] + ALIEN_SEPARATION, theAliens.getAliens().get(index).getPosY());
         }
     }
 }
