0cb384c719620d23d0e9e4ad6fd0f7a31856b76d
Adhemar Duran <adhemar.duran@fundacion-jala.org>
2022-03-06 00:46:04 -0400

    Refactor Play class, use Panel and Background classes

--- src/main/java/org/fundacionjala/at15/pacman/Play.java
+++ src/main/java/org/fundacionjala/at15/pacman/Play.java
@@ -1,13 +1,11 @@
 package org.fundacionjala.at15.pacman;
 
 import java.awt.Color;
-import java.awt.Image;
 import javax.swing.*;
 
 public class Play {
-    private JPanel gamePanel;
-    private JLabel gameBackground;
-    private ImageIcon imageGameBackground;
+    private Panel gamePanel;
+    private Background gameBackground;
     private Board board;
     private JLabel name;
     private int points;
@@ -24,20 +22,13 @@ public class Play {
     private final int ghostPos1 = 12;
     private final int ghostArray = 3;
 
+    public Play() {
+        gamePanel = new Panel();
+        gameBackground = new Background("src/main/java/org/fundacionjala/at15/pacman/images/playImage.jpg");
+    }
+
     public void runGame(Window window, String player, JPanel panelMenu) {
         panelMenu.setVisible(false);
-        gamePanel = new JPanel();
-        gamePanel.setLayout(null);
-        gamePanel.setBounds(0, 0, window.getWindow().getWidth(), window.getWindow().getHeight());
-        gamePanel.setVisible(true);
-
-        gameBackground = new JLabel();
-        gameBackground.setBounds(0, 0, window.getWindow().getWidth(), window.getWindow().getHeight());
-        imageGameBackground = new ImageIcon("src/main/java/org/fundacionjala/at15/pacman/images/playImage.jpg");
-        imageGameBackground = new ImageIcon(imageGameBackground.getImage()
-                .getScaledInstance(window.getWindow().getWidth(), window.getWindow().getHeight(), Image.SCALE_DEFAULT));
-        gameBackground.setIcon(imageGameBackground);
-        gameBackground.setVisible(true);
         gamePanel.add(gameBackground, 0);
 
         board = new Board();
@@ -70,7 +61,6 @@ public class Play {
         board.ghostMove(gamePanel, ghost2);
         board.ghostMove(gamePanel, ghost3);
         board.movPacman(window, gamePanel, records, panelMenu);
-
 
         window.getWindow().add(gamePanel);
     }
