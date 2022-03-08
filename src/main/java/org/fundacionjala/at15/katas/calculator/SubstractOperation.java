e75149be79d231b1e8bc8ff8a941280065ffcb64
Sergio <Sergio.Mendieta@fundacion-jala.org>
2022-03-08 09:46:56 -0400

    class SumOperationMultiple.java and its test added

--- src/main/java/org/fundacionjala/at15/katas/calculator/SubstractOperation.java
+++ src/main/java/org/fundacionjala/at15/katas/calculator/SubstractOperation.java
@@ -5,4 +5,7 @@ public class SubstractOperation extends Operation {
     public int run(int leftOperand, int rightOperand) {
         return leftOperand - rightOperand;
     }
+    public int run(int[] numbers) {
+        return 0;
+    }
 }
