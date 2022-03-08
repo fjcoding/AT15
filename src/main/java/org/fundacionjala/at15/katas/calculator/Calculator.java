dfd22b4b7f024766c2c4f70b258e883a0cf30148
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-03-08 10:14:46 -0300

    added recusive to function

--- src/main/java/org/fundacionjala/at15/katas/calculator/Calculator.java
+++ src/main/java/org/fundacionjala/at15/katas/calculator/Calculator.java
@@ -3,6 +3,7 @@ package org.fundacionjala.at15.katas.calculator;
 public class Calculator {
 
     public int doOperation(Operation operation, int leftOperand, int rightOperand) {
-        return operation.run(leftOperand, rightOperand);
+        leftOperand = operation.run(leftOperand, rightOperand);
+        return doOperation(operation, leftOperand, rightOperand);
     }
 }
