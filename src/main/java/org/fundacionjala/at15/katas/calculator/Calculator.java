4d91af8139f8b5f41f967b471ae56f77c5f950f5
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-03-08 23:26:27 -0300

    added multiple sum operations

--- src/main/java/org/fundacionjala/at15/katas/calculator/Calculator.java
+++ src/main/java/org/fundacionjala/at15/katas/calculator/Calculator.java
@@ -3,7 +3,15 @@ package org.fundacionjala.at15.katas.calculator;
 public class Calculator {
 
     public int doOperation(Operation operation, int leftOperand, int rightOperand) {
-        leftOperand = operation.run(leftOperand, rightOperand);
-        return doOperation(operation, leftOperand, rightOperand);
+        return operation.run(leftOperand, rightOperand);
+    }
+
+    public int doOperationChain(int leftOperand, Object[] arr, Operation operation) {
+        for (int index = 0; index < arr.length; index++) {
+            int rightOperand = (Integer) arr[index];
+            leftOperand = operation.run(leftOperand, rightOperand);
+        }
+
+        return leftOperand;
     }
 }
